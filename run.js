6d4fe391647d50f2769ad77712cdcc328d81759a
Anton Rusak <anton.rusak@gmail.com>
2017-03-06 15:22:08 +0300

    Смотреть несколько страниц с темами

--- run.js
+++ run.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const iconv = require('iconv-lite');
+const Rx = require('rx');
+
 const ignored = [
 	'91780', '81406', '84125'
 ];
@@ -8,20 +10,30 @@ const config = require('./config.json');
 const Mailer = require('./mailer');
 const mailer = new Mailer(config.mailer);
 
-http.get('http://www.astronomy.ru/forum/index.php/board,35.0/wap2.html', (res) => {
-	let chunks = [];
-	res.on('data', (chunk) => {
-		console.log(`Received ${chunk.length} bytes of data.`);
-		chunks.push(chunk);
-	});
-	res.on('end', () => {
+const placeholder = '%OFFSET%';
+const urlTemplate = `http://www.astronomy.ru/forum/index.php/board,35.${placeholder}/wap2.html`;
+
+Rx.Observable.from([0, 9, 18])
+	.map(offset => urlTemplate.replace(placeholder, offset))
+	.flatMap(url => {
+		return Rx.Observable.create(function (observer) {
+			http.get(url, res => {
+				res.on('data', chunk => observer.onNext(chunk))
+					.on('end', () => observer.onCompleted());
+			})
+		});
+	})
+	.reduce((acc, chunk) => {
+		acc.push(chunk);
+		return acc;
+	}, [])
+	.subscribe(chunks => {
 		let buffer = Buffer.concat(chunks);
 		let decoded = iconv.decode(buffer, 'cp1251');
 		let topics = processContent(decoded);
 
 		mailer.sendHtml(config.to, 'Новые объявления о продаже', makeHtml(topics));
-	})
-});
+	});
 
 function processContent(content) {
 	let reUrl = /href="(.+)\/wap2\.html/;
